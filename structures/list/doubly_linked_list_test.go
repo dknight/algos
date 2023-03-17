a8c22e6a7e1ae71f5c334485799abd22ca2e35c9
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2023-03-17 22:34:33 +0200

    feat(test): update doubly linked list tests.

--- structures/list/doubly_linked_list_test.go
+++ structures/list/doubly_linked_list_test.go
@@ -18,26 +18,36 @@ func TestList(t *testing.T) {
 	})
 
 	t.Run("DoublyLikedList_Empty()", func(t *testing.T) {
-		li := NewDoublyLikedList[int]()
-		exp := true
-		got := li.Empty()
-		if exp != got {
-			t.Errorf("Expected %v got %v", exp, got)
-		}
-		li.PushBack(1)
-		exp = false
-		got = li.Empty()
-		if exp != got {
-			t.Errorf("Expected %v got %v", exp, got)
-		}
-	})
+		t.Run("Common", func(t *testing.T) {
+			li := NewDoublyLikedList[int]()
+			exp := true
+			got := li.Empty()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+			li.PushBack(1)
+			exp = false
+			got = li.Empty()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+		})
 
-	t.Run("DoublyLikedList_Back() _Empty()", func(t *testing.T) {
-		li := NewDoublyLikedList[int]()
-		got := li.Back()
-		if got != nil {
-			t.Errorf("Expected %v got %v", nil, got)
-		}
+		t.Run("Back()", func(t *testing.T) {
+			li := NewDoublyLikedList[int]()
+			got := li.Back()
+			if got != nil {
+				t.Errorf("Expected %v got %v", nil, got)
+			}
+		})
+
+		t.Run("Front()", func(t *testing.T) {
+			li := NewDoublyLikedList[int]()
+			got := li.Front()
+			if got != nil {
+				t.Errorf("Expected %v got %v", nil, got)
+			}
+		})
 	})
 
 	t.Run("DoublyLikedList_Back()", func(t *testing.T) {
@@ -58,14 +68,6 @@ func TestList(t *testing.T) {
 		expFront := 111
 		if gotFront.Value() != expFront {
 			t.Errorf("Expected %v got %v", expFront, gotFront.Value())
-		}
-	})
-
-	t.Run("DoublyLinkedList_Front_Empty()", func(t *testing.T) {
-		li := NewDoublyLikedList[int]()
-		got := li.Front()
-		if got != nil {
-			t.Errorf("Expected %v got %v", nil, got)
 		}
 	})
 
@@ -96,76 +98,46 @@ func TestList(t *testing.T) {
 		}
 	})
 
-	t.Run("DoublyLinkedNode_Next()", func(t *testing.T) {
-		ranks := []string{
-			"private", "corporal", "sergeant", "lieutenant",
-		}
-		li := NewDoublyLikedList[string]()
-		for i := 0; i < len(ranks); i++ {
-			li.PushBack(ranks[i])
-		}
-		for i, node := 0, li.Front(); node != nil; i, node = i+1, node.Next() {
-			exp := ranks[i]
-			got := node.Value()
-			if exp != got {
-				t.Errorf("Expected %v got %v", exp, got)
-			}
-		}
-		for i, node := li.Len()-1, li.Back(); node != nil; i, node = i-1, node.Prev() {
-			exp := ranks[i]
-			got := node.Value()
-			if exp != got {
-				t.Errorf("Expected %v got %v", exp, got)
-			}
-		}
-	})
-
-	t.Run("DoublyLinkedNode_Prev()", func(t *testing.T) {
-		ranks := []string{
-			"private", "corporal", "sergeant", "lieutenant",
-		}
-		li := NewDoublyLikedList[string]()
-		for i := 0; i < len(ranks); i++ {
-			li.PushFront(ranks[i])
-		}
-		for i, nod := 0, li.Back(); nod != nil; i, nod = i+1, nod.Prev() {
-			exp := ranks[i]
-			got := nod.Value()
-			if exp != got {
-				t.Errorf("Expected %v got %v", exp, got)
-			}
-		}
-		for i, nod := li.Len()-1, li.Front(); nod != nil; i, nod = i-1, nod.Next() {
-			exp := ranks[i]
-			got := nod.Value()
-			if exp != got {
-				t.Errorf("Expected %v got %v", exp, got)
-			}
-		}
-	})
-
 	t.Run("DoublyLikedList_FindByValue()", func(t *testing.T) {
-		ranks := []string{
-			"private", "corporal", "sergeant", "lieutenant",
-		}
-		li := NewDoublyLikedList[string]()
-		for i := 0; i < len(ranks); i++ {
-			li.PushFront(ranks[i])
-		}
-		exp := ranks[2]
-		found := li.FindByValue(ranks[2])
-		got := found.Value()
-		if exp != got {
-			t.Errorf("Expected %v got %v", exp, got)
-		}
-	})
+		t.Run("Common", func(t *testing.T) {
+			ranks := []string{
+				"private", "corporal", "sergeant", "lieutenant",
+			}
+			li := NewDoublyLikedList[string]()
+			for i := 0; i < len(ranks); i++ {
+				li.PushFront(ranks[i])
+			}
+			exp := ranks[2]
+			found := li.FindByValue(ranks[2])
+			got := found.Value()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+		})
 
-	t.Run("DoublyLikedList_FindByValue_Empty()", func(t *testing.T) {
-		li := NewDoublyLikedList[string]()
-		got := li.FindByValue("Bambarbia")
-		if got != nil {
-			t.Errorf("Expected %v got %v", nil, got)
-		}
+		t.Run("Empty", func(t *testing.T) {
+			li := NewDoublyLikedList[string]()
+			got := li.FindByValue("Bambarbia")
+			if got != nil {
+				t.Errorf("Expected %v got %v", nil, got)
+			}
+		})
+
+		t.Run("structs", func(t *testing.T) {
+			type Person struct {
+				Name string
+				Aga  int
+			}
+			p1 := Person{"Toby", 25}
+			p2 := Person{"Toby", 25}
+
+			li := NewDoublyLikedList[Person]()
+			li.PushFront(p1)
+			found := li.FindByValue(p2)
+			if p1.Name != found.Value().Name {
+				t.Errorf("Expected %v got %v", p2, found.Value())
+			}
+		})
 	})
 
 	t.Run("DoublyLikedList_Remove()", func(t *testing.T) {
@@ -260,20 +232,54 @@ func TestList(t *testing.T) {
 			t.Errorf("Expected %v got %v", 0, li.Len())
 		}
 	})
+}
 
-	t.Run("DoublyLikedList_SearchByValue()_Struct", func(t *testing.T) {
-		type TestPerson struct {
-			Name string
-			Aga  int
+func TestDoublyLinkedNode(t *testing.T) {
+	t.Run("Next()", func(t *testing.T) {
+		ranks := []string{
+			"private", "corporal", "sergeant", "lieutenant",
 		}
-		p1 := TestPerson{"Toby", 25}
-		p2 := TestPerson{"Toby", 25}
+		li := NewDoublyLikedList[string]()
+		for i := 0; i < len(ranks); i++ {
+			li.PushBack(ranks[i])
+		}
+		for i, node := 0, li.Front(); node != nil; i, node = i+1, node.Next() {
+			exp := ranks[i]
+			got := node.Value()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+		}
+		for i, node := li.Len()-1, li.Back(); node != nil; i, node = i-1, node.Prev() {
+			exp := ranks[i]
+			got := node.Value()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+		}
+	})
 
-		li := NewDoublyLikedList[TestPerson]()
-		li.PushFront(p1)
-		found := li.FindByValue(p2)
-		if p1.Name != found.Value().Name {
-			t.Errorf("Expected %v got %v", p2, found.Value())
+	t.Run("Prev()", func(t *testing.T) {
+		ranks := []string{
+			"private", "corporal", "sergeant", "lieutenant",
+		}
+		li := NewDoublyLikedList[string]()
+		for i := 0; i < len(ranks); i++ {
+			li.PushFront(ranks[i])
+		}
+		for i, nod := 0, li.Back(); nod != nil; i, nod = i+1, nod.Prev() {
+			exp := ranks[i]
+			got := nod.Value()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
+		}
+		for i, nod := li.Len()-1, li.Front(); nod != nil; i, nod = i-1, nod.Next() {
+			exp := ranks[i]
+			got := nod.Value()
+			if exp != got {
+				t.Errorf("Expected %v got %v", exp, got)
+			}
 		}
 	})
 }
